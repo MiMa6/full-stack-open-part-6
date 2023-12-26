3b26c8647d0463108c5879e70bb7ed22261a325e
MM <michele.makiki@gmail.com>
2023-12-26 11:56:47 +0200

    Anecdotes and the backend, step3

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -3,15 +3,13 @@ import SearchFilter from './components/SearchFilter'
 import Notification from './components/Notification'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
-import anecdoteService from './services/anecdotes'
-import { setAnecdotes } from './reducers/anecdoteReducer'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
   const dispatch = useDispatch()
   useEffect(() => {
-    anecdoteService
-      .getAll().then(anecdotes => dispatch(setAnecdotes(anecdotes.concat())))
+    dispatch(initializeAnecdotes())
   }, [])
 
   const notification = useSelector(state => state.notification)
