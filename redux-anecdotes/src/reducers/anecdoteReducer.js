b74ceaa92505f6416cb1dfc0336c7baba9aeda86
MM <michele.makiki@gmail.com>
2023-12-26 11:28:12 +0200

    Anecdotes and the backend, step2

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -27,12 +27,7 @@ const anecdoteSlice = createSlice({
   initialState: [],
   reducers: {
     createAnecdote(state, action) {
-      const content = action.payload
-      state.push({
-        content,
-        id: getId(),
-        votes: 0
-      })
+      state.push(action.payload)
     },
     increaseVotes(state, action) {
       const id = action.payload
